557b44dbbca3b8d9a5a7dce016254cc84a7f55d0
Shirjeel Alam <shirjeel.alam@7vals.com>
2012-02-03 02:47:29 +0500

    User request specs passing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,17 +2,17 @@ require 'spec_helper'
 
 describe User do
   it "should not be valid without email" do
-  	user = Factory(:user, :email => nil)
+  	user = Factory.build(:user, :email => nil)
   	user.should_not be_valid
   end
 
   it "should not be valid without first name" do
-  	user = Factory(:user, :first_name => nil)
+  	user = Factory.build(:user, :first_name => nil)
   	user.should_not be_valid
   end
 
   it "should not be valid without last_name" do
-  	user = Factory(:user, :last_name => nil)
+  	user = Factory.build(:user, :last_name => nil)
   	user.should_not be_valid
   end
 end
