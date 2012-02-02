2ff27e24fcf65fb87c39e243fe4f8518b44ee458
Shirjeel Alam <shirjeel.alam@7vals.com>
2012-02-03 01:32:12 +0500

    Work on user spec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,15 @@
 # Read about factories at http://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
+	sequence :email do |n|
+    "email#{n}@jambul.com"
+  end
+
   factory :user do
+  	first_name 'John'
+    last_name  'Doe'
+    email { Factory.next(:email) }
+    password "foobar"
+    password_confirmation {|u| u.password}
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should not be valid without email" do
+  	user = Factory(:user, :email => nil)
+  	user.should_not be_valid
+  end
+
+  it "should not be valid without first name" do
+  	user = Factory(:user, :first_name => nil)
+  	user.should_not be_valid
+  end
+
+  it "should not be valid without last_name" do
+  	user = Factory(:user, :last_name => nil)
+  	user.should_not be_valid
+  end
 end
