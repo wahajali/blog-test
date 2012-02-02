70fe8cbe8615c77e12d58bd8996f9f06b3971710
Wahaj Ali <wahaj.ali@7vals.com>
2012-02-03 02:47:37 +0500

    Fixed post model specs

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,12 +3,12 @@ require 'spec_helper'
 describe Post do
 
  it "should not be valid without content" do
-   post = Factory(:post, :content => nil)
+   post = Factory.build(:post, :content => nil)
    post.should_not be_valid
  end
 
  it "should not be valid without user" do
-   post = Factory(:post, :user_id => nil)
+   post = Factory.build(:post, :user_id => nil)
    post.should_not be_valid
  end
 
